faf40690bcf63aca36bc4f2f527b62be7866db8b
yusunghyun616 <ysh616@svcorps.dev>
2020-12-20 19:07:42 +0900

    결과보기 1차 UI 및 기능 완료

--- src/Result/index.tsx
+++ src/Result/index.tsx
@@ -1,16 +1,80 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ToastAndroid,
+  View,
+} from 'react-native';
+import {DataType} from '../../Common/type';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import dayjs from 'dayjs';
+import scale from '../../Common/scale';
+import styles from './style';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 interface Props {
-  name: string;
+  route: {
+    params: {data: DataType; name: string};
+  };
+  navigation: StackNavigationProp<{}>;
 }
 
 export default function index(props: Props) {
+  const {route} = props;
+  const {params} = route;
+  const [data, set_data] = useState<DataType>({
+    ...params.data,
+  });
+  const [vote_volume,set_vote_volume] = useState<number>(0) 
+  useEffect(() => {
+    props.navigation.addListener('focus', async () => {
+      let tmp_cnt = 0;
+      params.data.item.forEach((v=>{
+        tmp_cnt += v.vote
+      }))
+      set_vote_volume(tmp_cnt)
+    });
+  }, [props.navigation]);
+
   return (
-    <View>
-      <Text>ㅎㅇㅎㅇㅎㅇ</Text>
+    <View style={{...styles.container}}>
+      <Text style={{...styles.title}}>{data.title}</Text>
+      <Text style={{...styles.sub_title}}>총 투표수 {vote_volume}</Text>
+      <FlatList
+        data={data?.item}
+        keyExtractor={(item, index) => index + 'd'}
+        renderItem={({item, index}) => {
+          return (
+            <>
+              <View
+                style={{...styles.input_check,flexDirection:'row',justifyContent:'space-between', marginHorizontal: scale(20)}}>
+                <Text style={{...styles.plust_text_check}}>{item.name}</Text>
+                <Text style={{...styles.plust_text_check}}>{item.vote}표 {item.vote ? Math.round(item.vote*100/vote_volume) : 0}%</Text>
+              </View>
+              {index === data?.item.length - 1 ? (
+                <>
+                  <View style={{flexDirection: 'row'}}>
+                    <Text style={{...styles.term_text, marginLeft: scale(20)}}>
+                      마감된 투표입니다.
+                    </Text>
+                  </View>
+                </>
+              ) : null}
+            </>
+          );
+        }}
+      />
+      <View style={{...styles.row_view}}>
+        <TouchableOpacity
+          onPress={() => {
+            props.navigation.goBack();
+          }}
+          style={{...styles.create_container}}>
+          <Text style={{...styles.create_txt}}>뒤로 가기!</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
--- src/Result/style.ts
+++ src/Result/style.ts
@@ -1,5 +1,167 @@
 import {Dimensions, StyleSheet} from 'react-native';
 import scale from '../../Common/scale';
 const {width: Width, height: Height} = Dimensions.get('window');
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFEFD5',
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: '#FFEFD5',
+    paddingHorizontal: scale(20),
+  },
+  row_view: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: scale(20),
+    fontWeight: 'bold',
+    margin: scale(20),
+    color: '#AE5E1A',
+  },
+  sub_title: {
+    alignSelf: 'flex-start',
+    fontSize: scale(20),
+    fontWeight: 'bold',
+    marginLeft: scale(20),
+    color: '#AE5E1A',
+  },
+  plust_text: {
+    fontSize: scale(14),
+    color: 'rgba(0,0,0,0.4)',
+    marginVertical: scale(10),
+  },
+  plust_text_check: {
+    fontSize: scale(14),
+    color: '#FFC091',
+    marginVertical: scale(10),
+  },
+  term_text: {
+    flex: 1,
+    fontSize: scale(14),
+    color: '#AE5E1A',
+  },
+  delete_container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  delete_touch: {
+    position: 'absolute',
+    top: scale(20),
+    right: scale(20),
+    backgroundColor: 'red',
+    elevation: 5,
+    borderRadius: scale(12),
+    padding: scale(5),
+    alignSelf: 'center',
+    marginBottom: scale(10),
+  },
+  delete_text: {
+    fontSize: scale(14),
+    color: '#ffffff',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#FFC091',
+    backgroundColor: '#FFDAB9',
+    borderRadius: scale(14),
+    paddingHorizontal: scale(10),
+    fontSize: scale(14),
+    color: '#000000',
+    marginBottom: scale(10),
+    elevation: 5,
+  },
+  input_check: {
+    borderWidth: 1,
+    borderColor: '#FFC091',
+    backgroundColor: '#AE5E1A',
+    borderRadius: scale(14),
+    paddingHorizontal: scale(10),
+    fontSize: scale(14),
+    color: '#000000',
+    marginBottom: scale(10),
+    elevation: 5,
+  },
+  flat_container: {
+    height: '85%',
+    backgroundColor: '#FFDAB9',
+    borderWidth: 1,
+    borderColor: '#FFC091',
+    marginHorizontal: scale(10),
+    borderRadius: scale(12),
+  },
+  vote_container: {
+    marginHorizontal: scale(20),
+    marginVertical: scale(10),
+    padding: scale(14),
+    borderRadius: scale(12),
+    backgroundColor: '#FFC091',
+    elevation: 5,
+  },
+  vote_row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  vote_txt: {
+    fontSize: scale(14),
+    fontWeight: 'bold',
+    color: '#AE5E1A',
+  },
+  vote_sub_txt: {
+    fontSize: scale(10),
+    color: '#C2722E',
+    alignSelf: 'flex-end',
+  },
+  vote_on: {
+    fontSize: scale(14),
+    fontWeight: 'bold',
+    color: '#AE5E1A',
+  },
+  vote_off: {
+    fontSize: scale(14),
+    fontWeight: 'bold',
+    color: '#AE5E1A',
+  },
+  create_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: scale(14),
+    marginHorizontal: scale(10),
+    marginLeft: scale(20),
+
+    marginTop: scale(20),
+    marginBottom: scale(30),
+    borderRadius: scale(12),
+    backgroundColor: '#FFB182',
+    elevation: 5,
+  },
+  create_container2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: scale(14),
+    marginHorizontal: scale(10),
+    marginRight: scale(20),
+    marginTop: scale(20),
+    marginBottom: scale(30),
+    borderRadius: scale(12),
+    backgroundColor: '#AE5E1A',
+    elevation: 5,
+  },
+  create_txt: {
+    fontSize: scale(14),
+    color: '#AE5E1A',
+    fontWeight: 'bold',
+  },
+  create_txt2: {
+    fontSize: scale(14),
+    color: '#FFB182',
+    fontWeight: 'bold',
+  },
+});
 export default styles;
